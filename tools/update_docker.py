198d63538cd3f3035f7249a04493a5106a67420b
Tsche <che@pydong.org>
2026-03-10 16:47:05 +0100

    update GH action, use ghcr

---
+++ tools/update_docker.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import subprocess
+
+REPO_PATH = Path(__file__).parent.parent
+
+def main():
+    subprocess.run(
+        ["docker", "build", "-f", REPO_PATH / ".devcontainer" / "Dockerfile", 
+         "-t", "ghcr.io/fuquery/antora:latest", "."],
+        check=True,
+        cwd=str(REPO_PATH))
+    
+    subprocess.run(
+        ["docker", "push", "ghrc.io/fuquery/antora:latest"],
+        check=True,
+        cwd=str(REPO_PATH))
+
+
+if __name__ == "__main__":
+    main()
